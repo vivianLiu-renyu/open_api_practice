6d48f627bb7f6aed632a5f562ce6065a533f27f8
Yenchen Huang <yenchenhuang@kkbox.com>
2019-01-11 12:22:58 +0800

    Create a dynamic playlist table view.

---
+++ open_api_practice/playlistData.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class playlistData {
+    var playlistName = ""
+    var curatorName = ""
+    
+    init(playlistName: String, curatorName: String) {
+        self.playlistName = playlistName
+        self.curatorName = curatorName
+    }
+}
---
+++ open_api_practice/playlistTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  playlistTableViewCell.swift
+//  open_api_practice
+//
+//  Created by YenChen Huang on 2019/1/11.
+//  Copyright © 2019年 Search_Question_Avengers. All rights reserved.
+//
+
+import UIKit
+
+class playlistTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var playlistName: UILabel!
+    @IBOutlet weak var curatorName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
