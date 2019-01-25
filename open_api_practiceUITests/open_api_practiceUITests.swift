d593a1ccc84d2e9f32fdfd10d2074d690e5cac1a
Ann Chu <annchu@Ann-Chus-MacBook.local>
2019-01-25 12:06:48 +0800

    Add Acceptance Test of story one

--- open_api_practiceUITests/open_api_practiceUITests.swift
+++ open_api_practiceUITests/open_api_practiceUITests.swift
@@ -26,9 +26,9 @@ class open_api_practiceUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
+    func testNewHitsPlaylistsHaveTwenty() {
+        let playlists = XCUIApplication().tables.cells
+        XCTAssert(playlists.count == 20)
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
+    } 
 }
