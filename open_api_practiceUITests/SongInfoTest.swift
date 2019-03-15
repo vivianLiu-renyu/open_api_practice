0984fa4062c01c523c1d43f18beb1a7b80d1a7b4
Ann Chu <annchu@kkbox.com>
2019-03-15 12:10:01 +0800

    rename NewHitsPlaylistTest

---
+++ open_api_practiceUITests/SongInfoTest.swift
@@ -0,0 +1,9 @@
+//
+//  SongInfoTest.swift
+//  open_api_practiceUITests
+//
+//  Created by Ann Chu on 2019/3/15.
+//  Copyright © 2019 Search_Question_Avengers. All rights reserved.
+//
+
+import Foundation
