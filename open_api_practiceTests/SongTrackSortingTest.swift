e3d5c4c5b2b58b6ed6bf875f1791ac6557dfce1a
yenchenhuang <yenchenhuang@kkbox.com>
2019-04-19 12:07:10 +0800

    Create a unit test target for the project.

---
+++ open_api_practiceTests/SongTrackSortingTest.swift
@@ -0,0 +1,33 @@
+//
+//  SongTrackSortingTest.swift
+//  open_api_practiceTests
+//
+//  Created by YenChen Huang on 2019/4/19.
+//  Copyright © 2019 Search_Question_Avengers. All rights reserved.
+//
+
+import XCTest
+
+class SongTrackSortingTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
